99e0ecca3f22238b076d0791c599114d2f66ba6b
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2020-05-21 20:12:12 -0300

    Create aritmetica.js

---
+++ aritmetica.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [2, 5, 7, 8]
+const soma = a + b + c + d
+const subtracao = a - b
+const multiplicacao = c * d
+const divisao = d / a
+const modulo = b % 3
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+

21f2f9e757743479cc1028e235069542eae82c57
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2020-05-21 20:17:08 -0300

    Create concatenacao.js

---
+++ concatenacao.js
@@ -0,0 +1,9 @@
+const nome = `camila tentando usar coisa nova`
+const segunda_tentativa = 'Ola' + ' ' + nome + '@'
+const template = `
+Oi ${nome} @`
+console.log(segunda_tentativa, template)
+
+//para saber se é verdadeiro ou falso  !! positivo ! false
+let testandoboleano = 1
+console.log(!!testandoboleano)

f1e3c3a3444c39dea52075baefb10bb01a5e5ac1
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2020-05-21 20:14:06 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+const valores = [6,9,10,11,12]
+console.log(valores[0], valores[4])
+console.log(valores.length)
+
+// para retirar valores do array ultimo valor
+console.log(valores.pop())
+console.log(valores)
+// para deletar um valor expecifico
+delete valores[0]
+console.log(valores)
+
+//cochetes representa array, já chaves objeto
+const[a] = [10]
+const [n1, n2, n3, n4, n5 = 0] = [10, 20, 30, 40, 50]
+
+

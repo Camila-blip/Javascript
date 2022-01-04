f2f552c0b3eb8d8e238e8e425b313f573a49b749
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-04 13:43:19 -0300

    Create switch.js

---
+++ ESNext/switch.js
@@ -0,0 +1,13 @@
+// se a variavel for igual ao case ele  executa a linha debaixo, se não ele vai descendo.
+var x = 2;
+switch(x){
+  case  0:
+      alert("o x vale 0 !");
+    break;
+  case 1:
+      alert("o x vale 1 !");
+    break;
+   case "string":
+      alert("x é uma string !");
+    break;
+};

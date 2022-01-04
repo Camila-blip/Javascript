31008faa7612108e4c063e973ce4c62ff91a9e64
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-04 12:13:10 -0300

    Create Lista.js

---
+++ Lista.js
@@ -0,0 +1,23 @@
+//Como encontrar algum elemento
+var lista = ["Camila", "Lucas", "Luiz", "Jhenifer", 15];
+
+lista.indexOf("Camila");
+
+//se eu der enter ele vai me devolver a posição que ele está, caso não exista ele me devolve uma posição negativa
+
+//Juntar uma lista
+lista.join(",");
+
+lista.join(" / ");
+
+//eliminar o último elemento da lista
+lista.pop();
+
+//eliminar o primeiro elemento da lista
+lista.shift();
+
+//Alterar um item
+lista[0] = "Camila Moura";
+
+//Adicionar um elemento na lista
+lista.push("Leonardo");

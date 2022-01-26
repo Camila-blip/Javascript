3affffb75eb975a6bd941e72886c689510e647f0
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-26 20:01:57 -0300

    Create Include.js

---
+++ Include.js
@@ -0,0 +1,12 @@
+//Procura o que eu passei 
+
+let nomes = ['Camila','Maria','Joao'];
+  console.log(nomes.includes('Maria'));
+
+let nome = 'Matheus';
+//Verifica se termina com determinada letra ou frases  ou algo do tipo
+  console.log(nome.endsWith('s'));
+
+let nome2 = 'CamilaMilla';
+    console.log(nome2.startsWith('Cam'));
+
